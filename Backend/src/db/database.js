32030c338111bef16850e0c63988625e4760d023
Krzysiek Durski <dursky.k@gmail.com>
2026-05-08 19:34:07 +0200

    Update database.js

--- Backend/src/db/database.js
+++ Backend/src/db/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: process.env.NODE_ENV === "production",
+      rejectUnauthorized: false,
     },
   },
 });
