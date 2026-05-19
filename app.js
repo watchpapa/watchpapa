6bed92f8e793def206f5af898ab393a4ac46d02f
Krzysiek Durski <dursky.k@gmail.com>
2026-05-19 22:12:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(helmet());
 
 app.use(express.json({ limit: "128kb" }));
 
-// Apply CORS headers for allowed frontend origins.
+// Apply CORS (Cross-origin resource sharing) headers for allowed frontend origins.
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin && corsAllowedOrigins.includes(origin)) {
